1efea1f993a88ee13bf8c0434f9251ef7435ea6b
KarmaSora <karammtr@gmail.com>
2024-09-12 08:20:54 +0200

    removed api folder, data now fetches from .json

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import Card from './Card'
+import fs from 'fs';
+import path from 'path';
+import Card from './Card';
 
-// Step 1: Define a TypeScript interface for the shape of the data
-interface ResearchItem {
-  title: string // The title of the research project
-  description: string // A brief description of the project
-  image: string // URL or path to the image representing the project
-  tags: string[] // An array of tags associated with the project
+interface ResearchData {
+  cardTitle: string;
+  cardDescription: string;
+  cardTags: string[];
+  cardImageURL: string;
 }
 
-// Step 2: Define the type for the props of the Container component
-// Here, "data" is an array of "ResearchItem" objects
-interface ContainerProps {
-  data: ResearchItem[] // "data" will be an array of ResearchItem objects
-}
+const Container = () => {
+  // Define the path to your JSON file
+  const jsonFilePath = path.join(process.cwd(), 'data', 'research.json');
+  
+  // Read the JSON file during build time
+  const data: ResearchData[] = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
 
-function Container({ data }: ContainerProps) {
   return (
-    <section className='card-container'>
+    <div>
       {data.map((item, index) => (
         <Card
           key={index}
-          cardTitle={item.title}
-          cardDescription={item.description}
-          cardTags={item.tags}
-          cardImageURL={item.image}
+          cardTitle={item.cardTitle}
+          cardDescription={item.cardDescription}
+          cardTags={item.cardTags}
+          cardImageURL={item.cardImageURL}
         />
       ))}
-    </section>
-  )
-}
+    </div>
+  );
+};
 
-export default Container
+export default Container;
