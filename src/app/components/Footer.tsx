d2669b52c102cc3d8f53ff4e2507679cd8c8d6fb
KarmaSora <karammtr@gmail.com>
2024-10-27 20:24:09 +0100

    added a Footer component, only shown in homepage

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <>
+      <footer className=' mx-auto bg-[#e8dcc0] p-4 shadow-lg'>
+        <div className='text-center'>
+          <p className='text-sm text-gray-800'>
+            &copy; 2024 <span className='font-bold'>showcaseSERL</span> All
+            Rights Reserved
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import Container from './components/Container'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className='container mx-auto bg-[#faf3e0] p-4'>
       <Header />
       <Container />
+      <Footer />
     </div>
   )
 }
